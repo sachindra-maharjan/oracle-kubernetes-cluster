a19accbcedf29cafd40deeebed2468eb63a799ed
Sachindra Maharjan <118582768+sachindra-maharjan@users.noreply.github.com>
2024-09-09 15:12:23 +0000

    Refactoring

--- oci-infra/infra.tf
+++ oci-infra/infra.tf
@@ -222,7 +222,7 @@ resource "oci_containerengine_node_pool" "k8s_node_pool" {
 
 resource "oci_artifacts_container_repository" "docker_repository" {
   compartment_id = var.compartment_id
-  display_name   = "free-kubernetes-nginx"
+  display_name   = "kubernetes-nginx"
 
   is_immutable = false
   is_public    = false
