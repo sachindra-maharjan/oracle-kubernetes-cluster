6e3262ed2454d8ba39d2d082aa6002441725d1ed
Sachindra Maharjan <118582768+sachindra-maharjan@users.noreply.github.com>
2024-09-11 21:17:22 +0000

    Updated newrelic helm values

--- k8s-mon/k8s-integration.sh
+++ k8s-mon/k8s-integration.sh
@@ -18,9 +18,10 @@ fi
 if [[ ${hasError} == 1 ]];then
     exit 1
 else 
-    KSM_IMAGE_VERSION="v2.10.0" && helm repo add newrelic https://helm-charts.newrelic.com && helm repo update && kubectl create namespace newrelic ; helm upgrade --install newrelic-bundle newrelic/nri-bundle --set global.licenseKey=${licenseKey} --set global.cluster=k8s-cluster --namespace=newrelic --set newrelic-infrastructure.privileged=true --set global.lowDataMode=true --set kube-state-metrics.image.tag=${KSM_IMAGE_VERSION} --set kube-state-metrics.enabled=true --set kubeEvents.enabled=true --set newrelic-prometheus-agent.enabled=true --set newrelic-prometheus-agent.lowDataMode=true --set newrelic-prometheus-agent.config.kubernetes.integrations_filter.enabled=false --set logging.enabled=true --set newrelic-logging.lowDataMode=false
+    KSM_IMAGE_VERSION="v2.10.0" && helm repo add newrelic https://helm-charts.newrelic.com && helm repo update && kubectl create namespace newrelic ; helm upgrade --install newrelic-bundle newrelic/nri-bundle --set global.licenseKey=${licenseKey} --set global.cluster=${clusterName} --namespace=newrelic --set newrelic-infrastructure.privileged=true --set global.lowDataMode=true --set kube-state-metrics.image.tag=${KSM_IMAGE_VERSION} --set kube-state-metrics.enabled=true --set kubeEvents.enabled=true --set newrelic-prometheus-agent.enabled=true --set newrelic-prometheus-agent.lowDataMode=true --set newrelic-prometheus-agent.config.kubernetes.integrations_filter.enabled=false --set logging.enabled=true --set newrelic-logging.lowDataMode=false
 
-    sed -i 's/<NEW_RELIC_LICENSE_KEY>/$NEW_RELIC_LICENSE_KEY/g' values.yaml
+    sed -i 's/<CLUSTER_NAME>/${clusterName}/g' values.yaml
+    sed -i 's/<NEW_RELIC_LICENSE_KEY>/${licenseKey}/g' values.yaml
 
     helm repo add newrelic https://helm-charts.newrelic.com && helm repo update && kubectl create namespace newrelic ; helm upgrade --install newrelic-bundle newrelic/nri-bundle -n newrelic --values values.yaml
 
