7825f384252667eb8f2f75827c4e02ad517b73d4
Sachindra Maharjan <118582768+sachindra-maharjan@users.noreply.github.com>
2024-09-24 18:56:41 +0000

    updated monitoring script

--- k8s-mon/k8s-integration.sh
+++ k8s-mon/k8s-integration.sh
@@ -27,7 +27,7 @@ if [[ ${hasError} == 1 ]];then
 else
     if [[ ${mode} == "install" ]]; then
         KSM_IMAGE_VERSION="v2.10.0" && helm repo add newrelic https://helm-charts.newrelic.com && helm repo update && kubectl create namespace newrelic ; helm upgrade --install newrelic-bundle newrelic/nri-bundle --set global.licenseKey=${licenseKey} --set global.cluster=${clusterName} --namespace=newrelic --set newrelic-infrastructure.privileged=true --set global.lowDataMode=true --set kube-state-metrics.image.tag=${KSM_IMAGE_VERSION} --set kube-state-metrics.enabled=true --set kubeEvents.enabled=true --set newrelic-prometheus-agent.enabled=true --set newrelic-prometheus-agent.lowDataMode=true --set newrelic-prometheus-agent.config.kubernetes.integrations_filter.enabled=false --set logging.enabled=true --set newrelic-logging.lowDataMode=true
-    else if [[ ${mode} == "update "]]; then
+    else if [[ ${mode} == "update" ]]; then
         sed -i "s/<CLUSTER_NAME>/${clusterName}/g" values.yaml
         sed -i "s/<NEW_RELIC_LICENSE_KEY>/${licenseKey}/g" values.yaml
 
